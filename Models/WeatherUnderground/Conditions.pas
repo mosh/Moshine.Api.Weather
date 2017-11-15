4c48d7fb270459af08c56405d237eaacb464f171
John Moshakis <JohnMoshakis@john-moshakiss-macbook-pro.local>
2017-11-14 22:20:47 -0500

    class per file

--- Models/WeatherUnderground/Conditions.pas
+++ Models/WeatherUnderground/Conditions.pas
@@ -13,53 +13,5 @@ type
     property Observation:Observation read _observation;
   end;
 
-  Observation = public class
-  private
-
-    method get_WindAsString: String;
-    begin
-
-      if(self.WindSpeedGusting > 0) then
-      begin
-        exit NSString.stringWithFormat('From the %@ at %d Gusting to %d Knots', self.WindDirection, self.WindSpeed, self.WindSpeedGusting);
-      end
-      else
-      begin
-        exit NSString.stringWithFormat('From the %@ at %d Knots', self.WindDirection,self.WindSpeed);
-      end;
-
-    end;
-
-    method get_ShortWindAsString:String;
-    begin
-      if(self.WindSpeedGusting > 0) then
-      begin
-        exit NSString.stringWithFormat('%@ %d Gusting %d Knts', self.WindDirection,self.WindSpeed,self.WindSpeedGusting);
-      end
-      else
-      begin
-        exit NSString.stringWithFormat('%@ %d Knts', self.WindDirection,self.WindSpeed);
-      end;
-    end;
-
-  public
-    property Weather:String;
-    property Temperature:Integer;
-    property WindDirection:String;
-    property WindDegress:Integer;
-    property WindSpeed:Integer;
-    property WindSpeedGusting:Integer;
-
-    property WindAsString:String read get_WindAsString;
-    property ShortWindAsString:String read get_ShortWindAsString;
-
-    constructor;
-    begin
-      self.Weather := '';
-      self.WindDirection := '';
-
-    end;
-
-  end;
 
 end.
---
+++ Models/WeatherUnderground/Observation.pas
@@ -0,0 +1,57 @@
+﻿namespace Moshine.Api.Weather.Models.WeatherUnderground;
+
+uses
+  Foundation;
+
+type
+
+  Observation = public class
+  private
+
+    method get_WindAsString: String;
+    begin
+
+      if(self.WindSpeedGusting > 0) then
+      begin
+        exit NSString.stringWithFormat('From the %@ at %d Gusting to %d Knots', self.WindDirection, self.WindSpeed, self.WindSpeedGusting);
+      end
+      else
+      begin
+        exit NSString.stringWithFormat('From the %@ at %d Knots', self.WindDirection,self.WindSpeed);
+      end;
+
+    end;
+
+    method get_ShortWindAsString:String;
+    begin
+      if(self.WindSpeedGusting > 0) then
+      begin
+        exit NSString.stringWithFormat('%@ %d Gusting %d Knts', self.WindDirection,self.WindSpeed,self.WindSpeedGusting);
+      end
+      else
+      begin
+        exit NSString.stringWithFormat('%@ %d Knts', self.WindDirection,self.WindSpeed);
+      end;
+    end;
+
+  public
+    property Weather:String;
+    property Temperature:Integer;
+    property WindDirection:String;
+    property WindDegress:Integer;
+    property WindSpeed:Integer;
+    property WindSpeedGusting:Integer;
+
+    property WindAsString:String read get_WindAsString;
+    property ShortWindAsString:String read get_ShortWindAsString;
+
+    constructor;
+    begin
+      self.Weather := '';
+      self.WindDirection := '';
+
+    end;
+
+  end;
+
+end.
